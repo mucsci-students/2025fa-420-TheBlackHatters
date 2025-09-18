bc908cb8ca0c3a0f478299423987874c4a2c9dae
Bhagi-Dhakal <Bhagirdhakal@gmail.com>
2025-09-18 18:11:17 -0400

    Upadats jsonFile, created Room Controler

--- CLI/main.py
+++ CLI/main.py
@@ -13,9 +13,10 @@
 import os #very dangerous
 import json
 from Models.Room_model import Room
-
+from CLI.room_cli import *
 # output/input Path
-outputPath = "output/example.json"
+outputPath = "output/example1.json"
+inputPath = "output/example.json"
 
 
 fileData = None
@@ -23,6 +24,9 @@ fileData = None
 # this is to parse the JSON file, putting each of the section into
 # their respective model 
 def parseJson(path):
+
+    # TODO: IF file is empty or difrent structur error and it will crash 
+    # we need to fix that...
     with open(path, 'r') as file:
         fileData = json.load(file)
 
@@ -34,20 +38,16 @@ def parseJson(path):
     Courses = config.get('courses')
     Faculty = config.get('faculty')
 
-    timeSlots = fileData.get("time_slot_config", {})
-    limit = fileData.get("limits", {})
-    optimizerFlags = fileData.get("optimizer_flags", {})
-
+    other = {
+        "time_slot_config": fileData.get("time_slot_config", {}),
+        "limits": fileData.get("limits", {}),
+        "optimizer_flags": fileData.get("optimizer_flags", {})
+    }
 
 
     # just return the others here as well. 
-    return Rooms, Labs, Courses, Faculty
+    return Rooms, Labs, Courses, Faculty, other
 
-# updates the json file with the changes made by user. 
-def updateJson(path, data):
-
-    with open(path, 'w') as file:
-        json.dump(data, file)
 
 # welcome the user and the options for our shell..
 def welcomeMessage():
@@ -62,40 +62,82 @@ def welcomeMessage():
 
     print("This program will parse your config files. ")
     print("With this program you can modify your config files. ")
-    print("You will be able to add, modify, remove:  faculty, courses, labs, Rooms." \
-    " Run and display the scheduler.")
+    print("You will be able to add, modify, remove: ")
+    print("faculty, courses, labs, Rooms.")
+    print("Run and display the scheduler.")
 
 
     # should be in its own function 
     print("\n")
     print("Please select one option: ")
-    print("1. View Current Congi File")
+    print("1. View Current Congfi File")
     print("2. Add, Modify, Delete Faculty")
     print("3. Add, Modify, Delete Rooms")
     print("4. Add, Modify, Delete Labs")
     print("5. Add, Modify, Delete Courses")
-    print("6. Save Changes") # maybe better for user if this happned automaticly 
-    print("7. Run Program")
+    print("6. Run Scheduler")
     print("0. Exit")
 
 
-# evethifor 
-def CLIRooms():
-    pass
-    # pass 
+# easier function to save daate
+def saveConfig(path, rooms, labs, courses, faculty, other):
+    newData = {
+        "config" : {
+            "rooms": rooms.rooms if hasattr(rooms, "rooms") else rooms,
+            "labs": labs,
+            "courses": courses,
+            "faculty": faculty,
+        },
+        "time_slot_config": other.get("time_slot_config", {}),
+        "limits": other.get("limits", {}),
+        "optimizer_flags": other.get("optimizer_flags", {})
+    }
 
+    with open(path, 'w') as file:
+        json.dump(newData, file, indent=4)
 
-
-
-
+    print("Changes have been saved! \n")
 
 # main function where everything will start form. 
 def main():
-    rooms, labs, courses, faculty = parseJson(outputPath)
+    rooms, labs, courses, faculty, other = parseJson(inputPath)
     
-    welcomeMessage()
+    while True:
+        welcomeMessage()
+        choice = input("Enter choice: ")
 
-    # call CLI functions.. 
+        if choice == "1":
+            input("Press Enter to continue...")
+        elif choice == "2":
+
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            input("Press Enter to continue...")
+        elif choice == "3":
+            mainRoomControler(rooms)
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            input("Press Enter to continue...")
+        elif choice == "4":
+
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            input("Press Enter to continue...")
+        elif choice == "5":
+
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            input("Press Enter to continue...")
+        elif choice == "6":
+
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            input("Press Enter to continue...")
+        elif choice == "0":
+            saveConfig(outputPath, rooms, labs, courses, faculty, other)
+            print("Goodbye!")
+            break
+        else:
+            print("Option not yet implemented.")
+            input("Press Enter to continue...")
+
+    quit()
+
 
 
 
--- CLI/room_cli.py
+++ CLI/room_cli.py
@@ -6,6 +6,8 @@
 #   With this we can add, modify, and remove rooms,
 #   make this changes directly to the json file. 
 
+def viewRooms(RoomModel):
+    return print(f"Rooms In System: {RoomModel.toJson()}.")
 
 # This will prompt the user to enter the name they want to add
 def addRoomCli(RoomModel):
@@ -13,6 +15,7 @@ def addRoomCli(RoomModel):
     name = input()
 
     RoomModel.add(name)
+    viewRooms(RoomModel)
 
 # This will prompt the user to enther the name they want to remove
 def removeRoomCli(RoomModel):
@@ -32,4 +35,29 @@ def modifyRoomCli(RoomModel):
 
 
 
+# ROOM CONTROLER! 
+def mainRoomControler(RoomModel):
 
+    while True:
+        print("\n--- Room Management ---")
+        print("1. View Rooms")
+        print("2. Add Room")
+        print("3. Delete Room")
+        print("4. Modify Room")
+        print("0. Back")
+        choice = input("Select: ")
+
+        if choice == "1":
+            viewRooms(RoomModel)
+        elif choice == "2":
+            addRoomCli(RoomModel)
+        elif choice == "3":
+            removeRoomCli(RoomModel)
+        elif choice == "4":
+            modifyRoomCli(RoomModel)
+        elif choice == "0":
+            return
+        else:
+            print("Invalid option. ")
+    
+    
